e3b3e4a6a80501a34570d03a502f26520ccb4ede
chocoboash <anleal2004@gmail.com>
2024-07-11 16:29:29 -0400

    Add comments to aid understanding of hardcodedDispenser

--- firmware/nano/hardcodedDispenser.ino
+++ firmware/nano/hardcodedDispenser.ino
@@ -11,32 +11,35 @@
 #include <Stepper.h>
 #include <Servo.h>
 
+// define pins
 const int buttonPin = 3;    
 const int ledPin = 9; 
 const int irSensorPin = 4; 
 const int servoPin1 = 5;
 const int servoPin2 = 6;
 
+// initialize variables
 volatile bool ledState = LOW;  
-
-// debounce prevents the button from being spammed
-const unsigned long debounceDelay = 250; // in ms
-volatile unsigned long lastDebounceTime = 0;  
-
 int resistorCount = 0; // count of resistors dispensed
 int requestedResistorCount = 1; // count of resistors requested, hardcoded to 1 for now
 
+// debounce prevents the push button from being spammed
+const unsigned long debounceDelay = 250; // in ms
+volatile unsigned long lastDebounceTime = 0;  
+
+// stepper motor configuration
 int stepsPerRevolution = 2048; // can be found in the motor's datasheet (this is for a 28BYJ-48 motor)
 const int revolutionsPerResistor = 1; // number of revolutions needed to dispense one resistor (Need to be calibrated)
 int motSpeed = 10; // speed in rpm 
-
 // IN1 to A4 (18), IN2 to A3 (17), IN3 to A2 (16), IN4 to A1 (15)
 Stepper myStepper(stepsPerRevolution, 18, 16, 17, 15);
 
+// servo motor configuration
 Servo myServo1;
 Servo myServo2;
 int servoPos = 0;
 
+// define states for the dispenser FSM
 enum DispenserState {
   IDLE,
   REELING,
@@ -45,10 +48,11 @@ enum DispenserState {
   COMPLETE
 };
 
-DispenserState dispenserState = IDLE; // set initial state 
+DispenserState dispenserState = IDLE; // set initial state of the dispenser 
 
 void setup() {
-  pinMode(buttonPin, INPUT_PULLUP);  
+  // initialize pins
+  pinMode(buttonPin, INPUT_PULLUP); // set the button pin as input with pullup resistor to prevent floating
   pinMode(irSensorPin, INPUT); 
   pinMode(ledPin, OUTPUT);  
 
@@ -62,6 +66,13 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(buttonPin), pushButtonISR, FALLING);
 
 }
+
+// in the main loop, we will check the state of the dispenser and act accordingly
+
+/*
+Finite State Machine (FSM):  model used to design algorithms that can be in one of a finite number of states at any given time. 
+The FSM transitions from one state to another based on inputs or events, and it can perform specific actions during these transitions or while in a particular state.
+*/
 
 void loop() {
     switch (dispenserState)
@@ -86,6 +97,17 @@ void loop() {
     }
 }
 
+/*
+Interrupt Service Routine (ISR): a callback function that is called when an interrupt occurs.
+Interrupts are used to handle time-sensitive tasks, such as reading sensor data or responding to user input.
+As opposed to polling, interrupts allow the microcontroller to respond immediately to events without wasting processing power.
+*/
+
+/*
+Debounce: a technique used to prevent mechanical switches or buttons from generating multiple signals when pressed or released.
+When a button is pressed, the contacts can bounce back and forth before settling into a stable state.
+*/
+
 // Interrupt Service Routine (ISR) for the button press
 void pushButtonISR() {
   unsigned long currentTime = millis();
@@ -98,6 +120,9 @@ void pushButtonISR() {
     }
   }
 }
+
+// The following functions are used to define the behavior of the dispenser in each state
+// They also transition the dispenser to the next state based on the FSM
 
 void reeling() {
   // move the motor enough for one resistor
