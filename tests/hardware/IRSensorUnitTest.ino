f8cbac0a554866fbec94c5f2fc29e036f04c7d54
chocoboash <anleal2004@gmail.com>
2024-07-11 02:19:00 -0400

    Added IR sensor unit test (untested)

---
+++ tests/hardware/IRSensorUnitTest.ino
@@ -0,0 +1,19 @@
+int irSensorPin = 4; 
+int ledPin = 13;
+
+void setup(){
+  pinMode(irSensorPin, INPUT); 
+  pinMode(ledPin, OUTPUT); 
+}
+
+void loop(){
+  int sensorStatus = digitalRead(irSensorPin);
+  if (sensorStatus == HIGH) // read sensor status
+  {
+    // if the pin is high turn off the onboard Led
+    digitalWrite(ledPin, LOW);
+  }
+  else  {
+    //else turn on the onboard LED
+    digitalWrite(ledPin, HIGH); 
+}
