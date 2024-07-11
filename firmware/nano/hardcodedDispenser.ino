6b11d7cebb5fb796b71a8e32a02fbe17f4acf907
chocoboash <anleal2004@gmail.com>
2024-07-11 16:32:01 -0400

    Fix formatting

--- firmware/nano/hardcodedDispenser.ino
+++ firmware/nano/hardcodedDispenser.ino
@@ -2,35 +2,35 @@
  * Project Name: Resistor Vending Machine
  * Author      : Ashley Leal
  * Date        : 07/11/2024
- * 
+ *
  * File Name   : hardcodedDispenser.ino
- * Purpose     : 
- * 
+ * Purpose     :
+ *
  */
 
-#include <Stepper.h>
 #include <Servo.h>
+#include <Stepper.h>
 
 // define pins
-const int buttonPin = 3;    
-const int ledPin = 9; 
-const int irSensorPin = 4; 
+const int buttonPin = 3;
+const int ledPin = 9;
+const int irSensorPin = 4;
 const int servoPin1 = 5;
 const int servoPin2 = 6;
 
 // initialize variables
-volatile bool ledState = LOW;  
-int resistorCount = 0; // count of resistors dispensed
-int requestedResistorCount = 1; // count of resistors requested, hardcoded to 1 for now
+volatile bool ledState = LOW;
+int resistorCount = 0;           // count of resistors dispensed
+int requestedResistorCount = 1;  // count of resistors requested, hardcoded to 1 for now
 
 // debounce prevents the push button from being spammed
-const unsigned long debounceDelay = 250; // in ms
-volatile unsigned long lastDebounceTime = 0;  
+const unsigned long debounceDelay = 250;  // in ms
+volatile unsigned long lastDebounceTime = 0;
 
 // stepper motor configuration
-int stepsPerRevolution = 2048; // can be found in the motor's datasheet (this is for a 28BYJ-48 motor)
-const int revolutionsPerResistor = 1; // number of revolutions needed to dispense one resistor (Need to be calibrated)
-int motSpeed = 10; // speed in rpm 
+int stepsPerRevolution = 2048;         // can be found in the motor's datasheet (this is for a 28BYJ-48 motor)
+const int revolutionsPerResistor = 1;  // number of revolutions needed to dispense one resistor (Need to be calibrated)
+int motSpeed = 10;                     // speed in rpm
 // IN1 to A4 (18), IN2 to A3 (17), IN3 to A2 (16), IN4 to A1 (15)
 Stepper myStepper(stepsPerRevolution, 18, 16, 17, 15);
 
@@ -41,59 +41,57 @@ int servoPos = 0;
 
 // define states for the dispenser FSM
 enum DispenserState {
-  IDLE,
-  REELING,
-  CUTTING,
-  COUNTING,
-  COMPLETE
+    IDLE,
+    REELING,
+    CUTTING,
+    COUNTING,
+    COMPLETE
 };
 
-DispenserState dispenserState = IDLE; // set initial state of the dispenser 
+DispenserState dispenserState = IDLE;  // set initial state of the dispenser
 
 void setup() {
-  // initialize pins
-  pinMode(buttonPin, INPUT_PULLUP); // set the button pin as input with pullup resistor to prevent floating
-  pinMode(irSensorPin, INPUT); 
-  pinMode(ledPin, OUTPUT);  
+    // initialize pins
+    pinMode(buttonPin, INPUT_PULLUP);  // set the button pin as input with pullup resistor to prevent floating
+    pinMode(irSensorPin, INPUT);
+    pinMode(ledPin, OUTPUT);
 
-  myServo1.attach(servoPin1);
-  myServo2.attach(servoPin2);    
+    myServo1.attach(servoPin1);
+    myServo2.attach(servoPin2);
 
-  // set the speed of the motor
-  myStepper.setSpeed(motSpeed);
+    // set the speed of the motor
+    myStepper.setSpeed(motSpeed);
 
-  // calling ISR on negedge of button press
-  attachInterrupt(digitalPinToInterrupt(buttonPin), pushButtonISR, FALLING);
-
+    // calling ISR on negedge of button press
+    attachInterrupt(digitalPinToInterrupt(buttonPin), pushButtonISR, FALLING);
 }
 
 // in the main loop, we will check the state of the dispenser and act accordingly
 
 /*
-Finite State Machine (FSM):  model used to design algorithms that can be in one of a finite number of states at any given time. 
+Finite State Machine (FSM):  model used to design algorithms that can be in one of a finite number of states at any given time.
 The FSM transitions from one state to another based on inputs or events, and it can perform specific actions during these transitions or while in a particular state.
 */
 
 void loop() {
-    switch (dispenserState)
-    {
-    case IDLE:
-      // do nothing, wait for button press
-      break;
-    case REELING:
-       reeling();
-       break;
-    case COUNTING:
-        counting();
-        break;
-    case CUTTING:
-       cutting();
-       break;
-    case COMPLETE:
-       complete();
-       break;
-    default:
-        break;
+    switch (dispenserState) {
+        case IDLE:
+            // do nothing, wait for button press
+            break;
+        case REELING:
+            reeling();
+            break;
+        case COUNTING:
+            counting();
+            break;
+        case CUTTING:
+            cutting();
+            break;
+        case COMPLETE:
+            complete();
+            break;
+        default:
+            break;
     }
 }
 
@@ -110,59 +108,58 @@ When a button is pressed, the contacts can bounce back and forth before settling
 
 // Interrupt Service Routine (ISR) for the button press
 void pushButtonISR() {
-  unsigned long currentTime = millis();
+    unsigned long currentTime = millis();
 
-  // if the current time is larger than the debounce delay, update the state to start reeling
-  if (currentTime - lastDebounceTime > debounceDelay) {
-    lastDebounceTime = currentTime; // update debounce
-    if (dispenserState == IDLE) {
-      dispenserState = REELING;
+    // if the current time is larger than the debounce delay, update the state to start reeling
+    if (currentTime - lastDebounceTime > debounceDelay) {
+        lastDebounceTime = currentTime;  // update debounce
+        if (dispenserState == IDLE) {
+            dispenserState = REELING;
+        }
     }
-  }
 }
 
 // The following functions are used to define the behavior of the dispenser in each state
 // They also transition the dispenser to the next state based on the FSM
 
 void reeling() {
-  // move the motor enough for one resistor
-  myStepper.step(stepsPerRevolution * revolutionsPerResistor);
-  dispenserState = COUNTING;
+    // move the motor enough for one resistor
+    myStepper.step(stepsPerRevolution * revolutionsPerResistor);
+    dispenserState = COUNTING;
 }
 
 void counting() {
-  int sensorStatus = digitalRead(irSensorPin);
-   if (sensorStatus == LOW) // read sensor status (is this active low?)
-   {
-    resistorCount++;
-   }
-   // now check if enough resistors have been reeled out
-    if (resistorCount == requestedResistorCount) {
-     dispenserState = CUTTING;
+    int sensorStatus = digitalRead(irSensorPin);
+    if (sensorStatus == LOW)  // read sensor status (is this active low?)
+    {
+        resistorCount++;
     }
-    else {
-      dispenserState = REELING;
+    // now check if enough resistors have been reeled out
+    if (resistorCount == requestedResistorCount) {
+        dispenserState = CUTTING;
+    } else {
+        dispenserState = REELING;
     }
 }
 
 void cutting() {
-    for(servoPos = 0; servoPos <= 180; servoPos+=1) {
-      myServo1.write(servoPos);
-      myServo2.write(servoPos);
+    for (servoPos = 0; servoPos <= 180; servoPos += 1) {
+        myServo1.write(servoPos);
+        myServo2.write(servoPos);
     }
     delay(500);
-    for(servoPos = 180; servoPos >= 0; servoPos-=1) {
-      myServo1.write(servoPos);
-      myServo2.write(servoPos);
+    for (servoPos = 180; servoPos >= 0; servoPos -= 1) {
+        myServo1.write(servoPos);
+        myServo2.write(servoPos);
     }
     dispenserState = COMPLETE;
 }
 
 void complete() {
-  // reset the servo positions
-  servoPos = 0;
-  // reset the resistor count
-  resistorCount = 0;
-  // reset the dispenser state
-  dispenserState = IDLE;
+    // reset the servo positions
+    servoPos = 0;
+    // reset the resistor count
+    resistorCount = 0;
+    // reset the dispenser state
+    dispenserState = IDLE;
 }
