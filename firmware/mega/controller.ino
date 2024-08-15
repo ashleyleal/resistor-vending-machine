27709cde6a33c7950c3393330f438a4820573717
chocoboash <anleal2004@gmail.com>
2024-08-14 23:57:36 -0400

    debugging...

--- firmware/mega/controller.ino
+++ firmware/mega/controller.ino
@@ -3,8 +3,8 @@
 #include <LiquidCrystal_I2C.h>
 
 // KEYPAD
-const byte ROWS = 4; 
-const byte COLS = 4; 
+const byte ROWS = 4;
+const byte COLS = 4;
 
 char keys[ROWS][COLS] = {
   {'1','2','3', 'A'},
@@ -12,15 +12,16 @@ char keys[ROWS][COLS] = {
   {'7','8','9', 'C'},
   {'*','0','#', 'D'}
 };
-byte rowPins[ROWS] = {37, 35, 33, 31}; 
-byte colPins[COLS] = {29, 27, 25, 23}; 
+byte rowPins[ROWS] = {37, 35, 33, 31};
+byte colPins[COLS] = {29, 27, 25, 23};
 
-Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 String textBuffer = "";
-char key = "";
+char key = '\0';  // Initialize key with '\0' (null character)
+char verifiedKey = '\0';  // Initialize verifiedKey with '\0' (null character)
 
 // LCD
-LiquidCrystal_I2C lcd(0x3F,16,2);  // set the LCD address to 0x3F for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x3F, 16, 2);  // set the LCD address to 0x3F for a 16 chars and 2 line display
 
 // Define Slave Select (SS) Pins for each Nano
 #define SS_NANO1 40
@@ -30,7 +31,7 @@ LiquidCrystal_I2C lcd(0x3F,16,2);  // set the LCD address to 0x3F for a 16 chars
 
 // define states for the master FSM
 enum MasterState {
-    IDLE,
+    MS_IDLE,
     SELECT_RESISTOR,
     CONFIRM_SELECTION,
     SELECT_QUANTITY,
@@ -38,31 +39,46 @@ enum MasterState {
     COMPLETE
 };
 
-MasterState masterState = IDLE;  // set initial state of the master
+MasterState masterState = MS_IDLE;  // set initial state of the master
 
 enum SelectedResistor {
-    A,
-    B,
-    C,
-    D,
+    RESISTOR_A,
+    RESISTOR_B,
+    RESISTOR_C,
+    RESISTOR_D,
     NONE
 };
 
 SelectedResistor selectedResistor = NONE;
 int resistorQuantity = 0;
 
-void setup(){
+void sweepLCD(String text, int row, int delayTime = 100) {
+    Serial.println(text);
+    int len = text.length();  
+    if (len <= 16) {  // If text fits on the screen, just print it
+        lcd.setCursor(0, row);   
+        lcd.print(text); 
+    } else {  // Text is longer than 16 characters, sweep it
+        for (int i = 0; i <= len - 16; i++) {
+            lcd.setCursor(0, row);
+            lcd.print(text.substring(i, i + 16));  // Display 16 characters at a time
+            delay(delayTime);  // Pause for a short period to create the sweeping effect
+        }
+    }
+}
+
+void setup() {
   Serial.begin(9600);
 
   // LCD SETUP
   lcd.init();           // Initialize the LCD
   lcd.clear();          // Clear the display
   lcd.backlight();      // Turn on the backlight
-  lcd.setCursor(0,0);   // Set cursor to the beginning
-  
+  lcd.setCursor(0, 0);  // Set cursor to the beginning
+
   // SPI SETUP
   SPI.begin();
-    // Set SS pins as OUTPUT and set them HIGH because SS is active low
+  // Set SS pins as OUTPUT and set them HIGH because SS is active low
   pinMode(SS_NANO1, OUTPUT);
   pinMode(SS_NANO2, OUTPUT);
   pinMode(SS_NANO3, OUTPUT);
@@ -71,13 +87,11 @@ void setup(){
   digitalWrite(SS_NANO2, HIGH);
   digitalWrite(SS_NANO3, HIGH);
   digitalWrite(SS_NANO4, HIGH);
-
-
 }
 
-void loop(){
+void loop() {
     switch(masterState) {
-        case IDLE:
+        case MS_IDLE:
             idle();
             break;
         case SELECT_RESISTOR:
@@ -87,114 +101,112 @@ void loop(){
             confirmSelection();
             break;
         case SELECT_QUANTITY:
-
+            selectQuantity();
             break;
         case DISPENSE_SIGNAL:
             dispenseSignal();
             break;
         case COMPLETE:
-
+            complete();
             break;
     }
 }
 
 void idle() {
-    printLCD("Welcome to the IEEE Resistor Vending Machine", 0);
-    printLCD("Please select a value (A-D)", 1);
+    sweepLCD("Welcome to the IEEE Resistor Vending Machine", 0);
+    sweepLCD("Choose A-D", 1);
     key = keypad.getKey();
     if (key == 'A' || key == 'B' || key == 'C' || key == 'D' || key == '*') {
+    
         masterState = SELECT_RESISTOR;
     }
 }
 
 void selectResistor() {
     // set selectedResistor
-    switch (key)
-    {
-    case 'A':
-        selectedResistor = A;
-        break;
-    case 'B':
-        selectedResistor = B;
-        break;
-    case 'C':
-        selectedResistor = C;
-        break;
-    case 'D':
-        selectedResistor = D;
-        break;
-    case '*':
-        masterState = IDLE; // go back to IDLE state
-        selectedResistor = NONE;
-        break;
+    switch (key) {
+        case 'A':
+            selectedResistor = RESISTOR_A;
+            break;
+        case 'B':
+            selectedResistor = RESISTOR_B;
+            break;
+        case 'C':
+            selectedResistor = RESISTOR_C;
+            break;
+        case 'D':
+            selectedResistor = RESISTOR_D;
+            break;
+        case '*':
+            masterState = MS_IDLE;  // go back to IDLE state
+            selectedResistor = NONE;
+            return;
     }
     masterState = CONFIRM_SELECTION;
 }
 
 void confirmSelection() {
     // print selected resistor and quantity
-    printLCD("You have selected dispenser" + selectedResistor, 0);
-    printLCD("Press '#' to confirm or '*' to choose again", 1);
+    sweepLCD("You have selected dispenser " + String(key), 0);
+    sweepLCD("Press '#' to confirm or '*' to choose again", 1);
     
     key = keypad.getKey();
     if (key == '#') {
         masterState = SELECT_QUANTITY;
     } else if (key == '*') {
-        masterState = IDLE;
+        masterState = MS_IDLE;
     }
 }
 
 void selectQuantity() {
-    // prompt quantity (5-10)
-    printLCD("How many resistors do you need?", 0);
-    printLCD("Enter a number between 4 and 10 and press '#' to continue", 1);
+    // prompt quantity (4-10)
+    sweepLCD("How many resistors do you need?", 0);
+    sweepLCD("Enter a number between 4 and 10 and press '#' to continue", 1);
 
     key = keypad.getKey();
 
     if (isDigit(key) || key == '*') {
-        handle_buffer();
+        handleBuffer();
     }
 
     if (key == '#') {
         if (verifyQuantity(4, 10)) {
             masterState = DISPENSE_SIGNAL;
         } else {
-            printLCD("Invalid quantity. Please enter a number between 4 and 10", 0);
+            sweepLCD("Invalid quantity. Please enter a number between 4 and 10", 0);
             
             delay(2000);
 
-            printLCD("How many resistors do you need?", 0);
-            printLCD("Enter a number between 4 and 10 and press '#' to continue", 1);
+            sweepLCD("How many resistors do you need?", 0);
+            sweepLCD("Enter a number between 4 and 10 and press '#' to continue", 1);
         }
     }
 }
 
 void dispenseSignal() {
-
     // send signal to selected Nano and print resistor value 
-    switch (selectedResistor)
-    {
-    case A:
-        sendSignal(SS_NANO1, selectQuantity);
-        break;
-    case B:
-        sendSignal(SS_NANO2, selectQuantity);
-        break;
-    case C:
-        sendSignal(SS_NANO3, selectQuantity);
-        break;
-    case D:
-        sendSignal(SS_NANO4, selectQuantity);
-        break;
+    switch (selectedResistor) {
+        case RESISTOR_A:
+            sendSignal(SS_NANO1, resistorQuantity);
+            break;
+        case RESISTOR_B:
+            sendSignal(SS_NANO2, resistorQuantity);
+            break;
+        case RESISTOR_C:
+            sendSignal(SS_NANO3, resistorQuantity);
+            break;
+        case RESISTOR_D:
+            sendSignal(SS_NANO4, resistorQuantity);
+            break;
     }
     masterState = COMPLETE;
 }
 
 void complete() {
-    printLCD("Dispensing " + resistorQuantity + " resistors", 0);
-    printLCD("Thank you for using the IEEE Resistor Vending Machine! :D", 1);
+    sweepLCD("Dispensing " + String(resistorQuantity) + " resistors", 0);
+    sweepLCD("Thank you for using the IEEE Resistor Vending Machine! :D", 1);
     delay(5000);
-    masterState = IDLE;
+    masterState = MS_IDLE;
     selectedResistor = NONE;
     resistorQuantity = 0; 
 }
@@ -205,20 +217,15 @@ void sendSignal(int ssPin, int quantity) {
     digitalWrite(ssPin, HIGH);           // Deselect the Nano
 }
 
-void printLCD(String text, int row) {
-    lcd.clear();  
-    lcd.setCursor(0, row);   
-    lcd.print(text); 
-}
-
 void handleBuffer() {
-  if (key == '*') { 
-    textBuffer = "";  // Clear the text buffer if '*' is pressed
-    lcd.clear();      // Clear the LCD display
-  } else {
-    textBuffer += key;  // Append the pressed key to the buffer
-    printLCD(textBuffer, 1);
-  }
+    if (key == '*') { 
+        textBuffer = "";  // Clear the text buffer if '*' is pressed
+        lcd.clear();      // Clear the LCD display
+    } else {
+        textBuffer += key;  // Append the pressed key to the buffer
+        sweepLCD(textBuffer, 1);
+        Serial.print(textBuffer);
+    }
 }
 
 bool verifyQuantity(int min, int max) {
@@ -230,3 +237,5 @@ bool verifyQuantity(int min, int max) {
         return false;
     }
 }
+
+
