8c3068b027eecaa62acbf6b46141b2d9a2261e75
chocoboash <anleal2004@gmail.com>
2024-08-16 17:35:33 -0400

    update keypad config in main code

--- firmware/mega/controller.ino
+++ firmware/mega/controller.ino
@@ -3,18 +3,17 @@
 #include <LiquidCrystal_I2C.h>
 
 // KEYPAD CONFIGURATION
-const byte ROWS = 4;
-const byte COLS = 4;
+const byte ROWS = 4; 
+const byte COLS = 4; 
 
 char keys[ROWS][COLS] = {
-    {'1', '2', '3', 'A'},
-    {'4', '5', '6', 'B'},
-    {'7', '8', '9', 'C'},
-    {'*', '0', '#', 'D'}
+  {'1','2','3', 'A'},
+  {'4','5','6', 'B'},
+  {'7','8','9', 'C'},
+  {'*','0','#', 'D'}
 };
-
-byte rowPins[ROWS] = {37, 35, 33, 31};
-byte colPins[COLS] = {29, 27, 25, 23};
+byte rowPins[ROWS] = {37, 35, 33, 31}; 
+byte colPins[COLS] = {29, 27, 25, 23}; 
 
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
